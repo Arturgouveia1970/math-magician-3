6b70b6c103ce26fac383102bc0d2ded61e3ba9a1
Arturgouveia1970 <artgouveiag@gmail.com>
2022-06-15 13:52:25 +0100

    styling

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -6,7 +6,7 @@ function Quotes() {
     <>
       <main className="quotesParagraph">
         <p className="paragraphQuote">
-          A man is like a fraction whose numerator is what he is
+          A Man is like a fraction whose numerator is what he is
           and whose denominator is what he thinks of himself.
           The larger the denominator the smaller the fraction.
           - Leo Tolstoy -
